8b16bb5a49325f5e77305924d3dc2268ef739ada
lewisab <1361087+lewisab@users.noreply.github.com>
2017-07-27 13:54:59 -0700

    Update vioplot.R

--- R/vioplot.R
+++ R/vioplot.R
@@ -1,5 +1,4 @@
-vioplot <- function(x,...,range=1.5,h=NULL,ylim=NULL,names=NULL, horizontal=FALSE, col="magenta", border="black", lty=1, lwd=1, rectCol="black", colMed="white", pchMed=19, at, add=FALSE, wex=1, drawRect=TRUE) 
-{
+vioplot <- function(x,...,range=1.5,h=NULL,ylim=NULL,names=NULL,horizontal=FALSE,col='magenta',border='black',lty=1,lwd=1,rectCol='black',colMed='white',pchMed=19,at,add=FALSE,wex=1,drawRect=TRUE){ 
   
   # process multiple datas
   datas <- list(x,...)
